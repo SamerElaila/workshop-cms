7dd373588dc8c815c107d96e3929fdfc75133462
samer elaila <samer-elaila@live.com>
2017-07-30 16:37:51 +0300

    enter relevant message

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 
+var ContentTypes = {
+  css: "text/css",
+  js : "application/javascript",
+  ico: 'image/x-icon',
+  png: "image/x-png",
+  jpg: "image/x-jpg"
+}
+
 var server =  http.createServer(handler);
 
 var message = "I am happy to to be part of the Node Ninjas Workshop!<br />Your url:";
@@ -23,10 +31,37 @@ function  handler(req , res){
   else if(url.toLowerCase() === "/girls"){
     res.end('Girls');
   }
+  // else if(url === "/public/"){
+  //
+  // }
   else{
-    res.end('Not Found!!');
+      // main.css & image.jpg
+      // console.log(__dirname + "/../public" + url);
+
+    fs.readFile(__dirname + '/../public'+url , function(err, data){
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.end(data);
+      }
+    });
   }
 };
+
+
+// if(url.startsWith("/public/")){
+//   var parts = url.split('.');
+//   var fileExtension = parts[parts.length -1];
+//   fs.readFile(__dirname + '/..' + url, function(err, data){
+//     if(err){
+//       console.log(err);
+//     }
+//     else{
+//       res.writeHead(200 , {'Content-Type': ContentTypes[fileExtension]});
+//       res.end(data);
+//     }
+//   });
 
 server.listen(3000, function(){
   console.log('Server is Listening on port 3000!');
