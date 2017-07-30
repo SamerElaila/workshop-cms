e4dd554f9fac24cf8785def68aa2d201c59cd1ab
samer elaila <samer-elaila@live.com>
2017-07-30 15:59:53 +0300

    enter relevant message

--- src/server.js
+++ src/server.js
@@ -1,13 +1,31 @@
 var http = require('http');
+var fs = require('fs');
 
 var server =  http.createServer(handler);
 
-var message = "I am happy to to be part of the Node Ninjas Workshop!";
+var message = "I am happy to to be part of the Node Ninjas Workshop!<br />Your url:";
 
-function  handler(request , response){
-  response.writeHead(200, {'Content-Type': "text/html"});
-  response.write(message);
-  response.end();
+function  handler(req , res){
+  var url = req.url;
+  if(url === "/"){
+    res.writeHead(200, {'Content-Type': "text/html"});
+    fs.readFile(__dirname + '/../public/index.html', function(err, data){
+      if(err){
+        consoel.log(err);
+      }else{
+        res.end(data);
+      }
+    });
+  }
+  else if(url.toLowerCase() === '/node'){
+    res.end('Node');
+  }
+  else if(url.toLowerCase() === "/girls"){
+    res.end('Girls');
+  }
+  else{
+    res.end('Not Found!!');
+  }
 };
 
 server.listen(3000, function(){
