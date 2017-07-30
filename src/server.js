6351010a5bfdc0266809c20c4444118dcfa73d6c
samer elaila <samer-elaila@live.com>
2017-07-30 15:38:41 +0300

    enter relevant messages

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
-var server =  http.createServer();
+var server =  http.createServer(handler);
+
+var message = "I am happy to to be part of the Node Ninjas Workshop!";
+
+function  handler(request , response){
+  response.writeHead(200, {'Content-Type': "text/html"});
+  response.write(message);
+  response.end();
+};
 
 server.listen(3000, function(){
   console.log('Server is Listening on port 3000!');
